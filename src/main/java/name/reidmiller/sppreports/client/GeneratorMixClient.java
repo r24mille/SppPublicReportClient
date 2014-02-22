9c7d1f3ce8f3bbb27b929ff963de2ed041525207
Reid Miller <r24mille@uwaterloo.ca>
2014-02-22 12:54:00 -0500

    Commented GeneratorMixClient up with documentation.

--- src/main/java/name/reidmiller/sppreports/client/GeneratorMixClient.java
+++ src/main/java/name/reidmiller/sppreports/client/GeneratorMixClient.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.TreeSet;
@@ -28,21 +27,41 @@ import au.com.bytecode.opencsv.CSVReader;
 public class GeneratorMixClient {
 	public static final String GENERATOR_MIX_REPORT_DATE_FORMAT = "M/d/yyyy H:mm";
 	private Logger logger = LogManager.getLogger(this.getClass());
-	private DateTimeFormatter dateTimeFormat;
+	private DateTimeFormatter centralTimeFormat;
+	private DateTimeZone usCentralZone;
 
+	/**
+	 * GeneratorMixClient constructor sets {@link #centralTimeFormat} using
+	 * {@value #GENERATOR_MIX_REPORT_DATE_FORMAT} and {@link #usCentralZone}.
+	 */
 	GeneratorMixClient() {
+		usCentralZone = DateTimeZone.forID("America/Chicago");
 		DateTimeFormatter localDateTimeFormat = DateTimeFormat
 				.forPattern(GENERATOR_MIX_REPORT_DATE_FORMAT);
-		this.dateTimeFormat = localDateTimeFormat.withZone(DateTimeZone
-				.forID("America/Chicago"));
+		this.centralTimeFormat = localDateTimeFormat.withZone(usCentralZone);
 	}
 
+	/**
+	 * @param samplingFrequency
+	 *            Either five-minute or hourly report.
+	 * @return List of {@link GeneratorMix} objects from the start of the
+	 *         current year to the current date.
+	 */
 	public List<GeneratorMix> getDefaultGeneratorMixes(
 			SamplingFrequency samplingFrequency) {
-		return this.getGenMixesForYear(Calendar.getInstance()
-				.get(Calendar.YEAR), samplingFrequency);
+		return this.getGenMixesForYear(DateTime.now().getYear(),
+				samplingFrequency);
 	}
 
+	/**
+	 * @param year
+	 *            Year of report to request.
+	 * @param samplingFrequency
+	 *            Switches the report URL String between five-minute and hourly
+	 *            using {@link SamplingFrequency#getUrlPart()}.
+	 * @return SPP GenerationMix report URL string stitched together based on
+	 *         parameters passed to method.
+	 */
 	public String getUrlString(int year, SamplingFrequency samplingFrequency) {
 		String urlString = "http://www.spp.org/GenerationMix/" + year + "_"
 				+ samplingFrequency.getUrlPart() + "_GenMix.csv";
@@ -50,16 +69,29 @@ public class GeneratorMixClient {
 		return urlString;
 	}
 
+	/**
+	 * Iterates over time range specified and repeatedly calls
+	 * {@link #getGenMixesForYear(int, SamplingFrequency)} to build out the List
+	 * of {@link GeneratorMix} objects. Items outside of date range are filtered
+	 * out if necessary.
+	 * 
+	 * @param samplingFrequency
+	 * 
+	 * @param startDate
+	 *            Lower bound of {@link GeneratorMix} objects in the List
+	 *            returned.
+	 * @param endDate
+	 *            Upper bound of {@link GeneratorMix} objects in the List
+	 *            returned.
+	 * @return List of {@link GeneratorMix} objects in the specified date range.
+	 */
 	public List<GeneratorMix> getGeneratorMixesInRange(
 			SamplingFrequency samplingFrequency, Date startDate, Date endDate) {
-		Calendar startCal = Calendar.getInstance();
-		startCal.setTime(startDate);
-		Calendar endCal = Calendar.getInstance();
-		endCal.setTime(endDate);
+		DateTime startDateTime = new DateTime(startDate);
+		DateTime endDateTime = new DateTime(endDate);
 
 		TreeSet<Integer> yearRange = new TreeSet<Integer>();
-		for (int y = startCal.get(Calendar.YEAR); y <= endCal
-				.get(Calendar.YEAR); y++) {
+		for (int y = startDateTime.getYear(); y <= endDateTime.getYear(); y++) {
 			yearRange.add(y);
 		}
 
@@ -68,24 +100,23 @@ public class GeneratorMixClient {
 			List<GeneratorMix> yearGenMixes = this.getGenMixesForYear(year,
 					samplingFrequency);
 
-			if (year > startCal.get(Calendar.YEAR)
-					&& year < endCal.get(Calendar.YEAR)) {
+			if (year > startDateTime.getYear() && year < endDateTime.getYear()) {
 				generatorMixes.addAll(yearGenMixes);
-			} else if (year == startCal.get(Calendar.YEAR)
-					&& year == endCal.get(Calendar.YEAR)) {
+			} else if (year == startDateTime.getYear()
+					&& year == endDateTime.getYear()) {
 				for (GeneratorMix genMix : yearGenMixes) {
 					if (genMix.getDate().compareTo(startDate) >= 0
 							&& genMix.getDate().compareTo(endDate) <= 0) {
 						generatorMixes.add(genMix);
 					}
 				}
-			} else if (year == startCal.get(Calendar.YEAR)) {
+			} else if (year == startDateTime.getYear()) {
 				for (GeneratorMix genMix : yearGenMixes) {
 					if (genMix.getDate().compareTo(startDate) >= 0) {
 						generatorMixes.add(genMix);
 					}
 				}
-			} else if (year == endCal.get(Calendar.YEAR)) {
+			} else if (year == endDateTime.getYear()) {
 				for (GeneratorMix genMix : yearGenMixes) {
 					if (genMix.getDate().compareTo(endDate) <= 0) {
 						generatorMixes.add(genMix);
@@ -97,14 +128,32 @@ public class GeneratorMixClient {
 		return generatorMixes;
 	}
 
+	/**
+	 * Method retrieves the GeneratorMix objects for a given year. Because the
+	 * CSV provides times without offset or daylight savings information, this
+	 * method does a bit of work to find and correct the error, creating
+	 * {@link Date} objects with proper offset information.
+	 * 
+	 * @param year
+	 *            Year of report.
+	 * @param samplingFrequency
+	 *            Five-minute or hourly sampling frequency controls which report
+	 *            URL is created. Passed to
+	 *            {@link #getUrlString(int, SamplingFrequency)} along with year.
+	 * @return List of {@link GeneratorMix} objects for the year.
+	 */
 	public List<GeneratorMix> getGenMixesForYear(int year,
 			SamplingFrequency samplingFrequency) {
-		DateTimeZone usCentral = DateTimeZone.forID("America/Chicago");		
-		DateTime august1st = new DateTime(year, 8, 1, 0, 0, 0, 0, usCentral);
-		Instant cdtToCst = new Instant(usCentral.nextTransition(august1st
+		// An arbitrary date known to be in the middle of daylight savings.
+		DateTime august1st = new DateTime(year, 8, 1, 0, 0, 0, 0, usCentralZone);
+		// Instant of transition from daylight savings to standard time.
+		Instant cdtToCst = new Instant(usCentralZone.nextTransition(august1st
 				.toInstant().getMillis()));
+		// Instant of last daylight savings sample in the report.
 		Instant lastCdt = null;
+		// When last sample in DST has been hit, trigger standard time fix.
 		boolean startCstFix = false;
+		// Number of records to fix changes for five-minute or hourly report.
 		int numCstFixed = 0;
 		int cstFixLimit = 0;
 		switch (samplingFrequency) {
@@ -117,7 +166,8 @@ public class GeneratorMixClient {
 			cstFixLimit = 1;
 			break;
 		}
-		logger.debug("Last sample of CDT is " + lastCdt.toDateTime(usCentral));
+		logger.debug("Last sample of CDT is "
+				+ lastCdt.toDateTime(usCentralZone));
 
 		List<GeneratorMix> generatorMixes = new ArrayList<GeneratorMix>();
 		String urlString = this.getUrlString(year, samplingFrequency);
@@ -127,24 +177,31 @@ public class GeneratorMixClient {
 					url.openStream()));
 			CSVReader reader = new CSVReader(in);
 
+			// Loop over each row of CSV report
 			String[] csvLine = reader.readNext();
 			for (int i = 0; csvLine != null; i++) {
+				// Skip first row and empty rows
 				if (i > 0 && csvLine[0] != null && !csvLine[0].isEmpty()) {
 					GeneratorMix generatorMix = new GeneratorMix();
-					DateTime genMixDateTime = dateTimeFormat
+					DateTime genMixDateTime = centralTimeFormat
 							.parseDateTime(csvLine[0]);
 
+					// If DST flag has been set and fix count is under limit
 					if (startCstFix && numCstFixed < cstFixLimit) {
 						Instant incorrectCst = genMixDateTime.toInstant();
-						Instant correctCdt = incorrectCst.plus(Duration.standardHours(1));
-						logger.debug("Incorrect Date " + incorrectCst.toDateTime(usCentral) + " corrected to " + correctCdt.toDateTime(usCentral));
+						Instant correctCdt = incorrectCst.plus(Duration
+								.standardHours(1));
+						logger.debug("Incorrect Date "
+								+ incorrectCst.toDateTime(usCentralZone)
+								+ " corrected to "
+								+ correctCdt.toDateTime(usCentralZone));
 						generatorMix.setDate(correctCdt.toDate());
 						numCstFixed++;
 					} else {
 						generatorMix.setDate(genMixDateTime.toDate());
-						startCstFix = false;
 					}
-					
+
+					// When last DST row hit, trigger fix to tart next iteration
 					if (genMixDateTime.isEqual(lastCdt)) {
 						startCstFix = true;
 					}
